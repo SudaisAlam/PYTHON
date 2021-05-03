f40006dfd27fcf2642763ce30d8ca8c68529b466
SudaisAlam <alamsudais72@gmail,com>
2021-05-04 00:55:06 +0500

    First commit

---
+++ string.py
@@ -0,0 +1,17 @@
+
+x = """ This is sudais haidar alam reporting.
+he's writing python code 
+and this is test. """
+
+print(x)
+
+y = ''' This is sudais haidar alam again
+he's using single quotation this time.'''
+
+print(y)
+
+#print(x[3])
+#print(y[23])
+
+print(len(x))
+print(len(y))
